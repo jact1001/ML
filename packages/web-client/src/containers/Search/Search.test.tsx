85b92af965dee92242b38236ceb345510aa92bb0
johnnychinchajoa <jhony.chinchajoa@avaldigitallabs.com>
2021-12-06 15:42:27 -0500

    :bento: fixed unit test

--- packages/web-client/src/containers/Search/Search.test.tsx
+++ packages/web-client/src/containers/Search/Search.test.tsx
@@ -3,15 +3,20 @@ import {Search} from "./Search";
 import { Router } from 'react-router-dom';
 import {mount} from 'enzyme';
 import {createMemoryHistory} from "history";
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store'
+const mockStore = configureStore();
 
-describe('search test',()=>{
+describe('search tests',()=>{
     const history = createMemoryHistory();
     const route = '/items/MLA123654895';
     history.push(route);
     const wrapper = mount(
-        <Router history={history}>
-            <Search/>
-        </Router>
+        <Provider store={mockStore()}>
+            <Router history={history}>
+                <Search/>
+            </Router>
+        </Provider>,
     );
     xtest('retorna el texto del input',()=>{
         const search = wrapper.children().find('input').simulate('keypress', {key: 'Enter'})
